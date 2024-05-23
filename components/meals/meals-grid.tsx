63b993a492330725fc31d412f878d54c4b95f5de
garold664 <garold664@gmail.com>
2024-05-23 16:18:07 +0300

    created page for individual meals

--- components/meals/meals-grid.tsx
+++ components/meals/meals-grid.tsx
@@ -1,15 +1,7 @@
 import MealItem from './meal-item';
 import styles from './meals-grid.module.css';
 
-export type Meal = {
-  id: string;
-  title: string;
-  slug: string;
-  summary: string;
-  image: string;
-  creator: string;
-  instructions: string;
-};
+import { Meal } from '@/lib/meals';
 
 export default function MealsGrid({ meals }: { meals: Meal[] }) {
   return (
--- lib/meals.ts
+++ lib/meals.ts
@@ -1,8 +1,23 @@
 import sql from 'better-sqlite3';
 const db = sql('meals.db');
 
+export type Meal = {
+  id: string;
+  title: string;
+  slug: string;
+  summary: string;
+  image: string;
+  creator: string;
+  creator_email: string;
+  instructions: string;
+};
+
 export default async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   // throw new Error('test error');
-  return db.prepare('SELECT * FROM meals').all();
+  return db.prepare('SELECT * FROM meals').all() as Meal[];
+}
+
+export function getMeal(slug: string) {
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug) as Meal;
 }
