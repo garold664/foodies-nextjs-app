e735ec3764d552191ab4ecdbac6846c1daebb624
garold664 <garold664@gmail.com>
2024-06-07 19:02:01 +0300

    added images config in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { hostname } from 'os';
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        port: '',
+        pathname: '/dudevjtfp/image/upload/**',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+};
 export default nextConfig;
